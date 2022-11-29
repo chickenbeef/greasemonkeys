20ab91dab6259e4df1c399dd2f94d1a468865fa0
Iftekhar Akoob <chickenbeef@users.noreply.github.com>
2022-11-29 17:09:28 +0200

    Create ebucks40.js

---
+++ ebucks40.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         eBucks 40%
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Highlight 40% eBucks discounts
+// @author       You
+// @match        https://www.ebucks.com/web/shop/categorySelected.do?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=ebucks.com
+// @grant        none
+// ==/UserScript==
+
+setTimeout(highlightWord, 3000);
+// Wait for slow 40% off to load
+setTimeout(highlightWord, 5000);
+
+function highlightWord() {
+    console.log("Highlighting");
+    document.body.innerHTML = document.body.innerHTML.replace(/Get up to 40% off/g, function(m){
+        return '<span style="background-color:yellow">'+m+'</span>'
+    });
+}
+
+
